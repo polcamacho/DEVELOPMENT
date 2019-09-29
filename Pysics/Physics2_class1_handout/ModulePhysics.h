6b82b14f771260ab1df8b44b9c591a9d72ee351d
polcamacho <47743832+polcamacho@users.noreply.github.com>
2019-09-29 19:59:03 +0200

    it lasts macros meters to pixels

---
+++ Pysics/Physics2_class1_handout/ModulePhysics.h
@@ -0,0 +1,28 @@
+#pragma once
+#include "Module.h"
+#include "Globals.h"
+#include "Box2D/Box2D/Box2D.h"
+
+class ModulePhysics : public Module
+{
+public:
+	ModulePhysics(Application* app, bool start_enabled = true);
+	~ModulePhysics();
+
+	bool Start();
+	update_status PreUpdate();
+	update_status PostUpdate();
+	bool CleanUp();
+	b2World* World = nullptr;
+	b2BodyDef bodydef;
+	b2Body* body = nullptr;
+	float32 timeStep = 1.0f / 60.0f;
+	int32 velocityiterator = 8;
+	int32 positioniterator = 3;
+
+
+private:
+
+
+	bool debug;
+};
